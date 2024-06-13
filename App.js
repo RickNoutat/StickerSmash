4a4dae1a0687f2e62c3f0b4365f818e9c23f2f53
RickyDavinci <ricknoutat@gmail.com>
2024-06-13 09:25:23 +0100

    fourth commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 
-import ImageViewer from "./components/ImageViewer.js";
+import Button from "./components/Button";
+import ImageViewer from "./components/ImageViewer";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
@@ -11,25 +12,19 @@ export default function App() {
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choisir cette photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
+  // Styles that are unchanged from previous step are hidden for brevity
+  footerContainer: {
+    flex: 1 / 3,
     alignItems: "center",
-    justifyContent: "center",
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
 });
